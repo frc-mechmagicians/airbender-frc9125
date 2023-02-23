de9ebfd34501ae177dc967c0512e334d2749dd4d
akashdanda <79346459+akashdanda@users.noreply.github.com>
2023-02-22 17:04:06 -0800

    Update Robot2.java

--- src/main/java/frc/robot/Robot2.java
+++ src/main/java/frc/robot/Robot2.java
@@ -1,180 +1,317 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot;
 
-import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
-import com.revrobotics.RelativeEncoder;
-import com.revrobotics.SparkMaxPIDController;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-/**
- * Before Running:
- * Open shuffleBoard, select File->Load Layout and select the 
- * shuffleboard.json that is in the root directory of this example
- */
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-/**
- * REV Smart Motion Guide
- * 
- * The SPARK MAX includes a new control mode, REV Smart Motion which is used to 
- * control the position of the motor, and includes a max velocity and max 
- * acceleration parameter to ensure the motor moves in a smooth and predictable 
- * way. This is done by generating a motion profile on the fly in SPARK MAX and 
- * controlling the velocity of the motor to follow this profile.
- * 
- * Since REV Smart Motion uses the velocity to track a profile, there are only 
- * two steps required to configure this mode:
- *    1) Tune a velocity PID loop for the mechanism
- *    2) Configure the smart motion parameters
- * 
- * Tuning the Velocity PID Loop
- * 
- * The most important part of tuning any closed loop control such as the velocity 
- * PID, is to graph the inputs and outputs to understand exactly what is happening. 
- * For tuning the Velocity PID loop, at a minimum we recommend graphing:
- *
- *    1) The velocity of the mechanism (‘Process variable’)
- *    2) The commanded velocity value (‘Setpoint’)
- *    3) The applied output
- *
- * This example will use ShuffleBoard to graph the above parameters. Make sure to
- * load the shuffleboard.json file in the root of this directory to get the full
- * effect of the GUI layout.
- */
-public class Robot2 extends TimedRobot {
-  private static final int deviceID = 1;
-  private CANSparkMax m_motor;
-  private SparkMaxPIDController m_pidController;
-  private RelativeEncoder m_encoder;
-  public double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput, maxRPM, maxVel, minVel, maxAcc, allowedErr;
+public class Robot extends TimedRobot {
+  /*
+   * Autonomous selection options.
+   */
+  private static final String kNothingAuto = "do nothing";
+  private static final String kConeAuto = "cone";
+  private static final String kCubeAuto = "cube";
+  private String m_autoSelected;
+  private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
+  /*
+   * Drive motor controller instances.
+   * 
+   * Change the id's to match your robot.
+   * Change kBrushed to kBrushless if you are using NEO's.
+   * Use the appropriate other class if you are using different controllers.
+   */
+  CANSparkMax driveLeftSpark = new CANSparkMax(1, MotorType.kBrushless);
+  CANSparkMax driveRightSpark = new CANSparkMax(3, MotorType.kBrushless);
+  CANSparkMax driveLeftSpark2 = new CANSparkMax(2, MotorType.kBrushless);
+  CANSparkMax driveRightSpark2 = new CANSparkMax(4, MotorType.kBrushless);
+
+  /*
+   * Mechanism motor controller instances.
+   * 
+   * Like the drive motors, set the CAN id's to match your robot or use different
+   * motor controller classses (TalonFX, TalonSRX, Spark, VictorSP) to match your
+   * robot.
+   * 
+   * The arm is a NEO on Everybud.
+   * The intake is a NEO 550 on Everybud.
+   */
+  CANSparkMax arm = new CANSparkMax(5, MotorType.kBrushless);
+  CANSparkMax intake = new CANSparkMax(6, MotorType.kBrushless);
+
+  /**
+   * The starter code uses the most generic joystick class.
+   * 
+   * The reveal video was filmed using a logitech gamepad set to
+   * directinput mode (switch set to D on the bottom). You may want
+   * to use the XBoxController class with the gamepad set to XInput
+   * mode (switch set to X on the bottom) or a different controller
+   * that you feel is more comfortable.
+   */
+  Joystick j = new Joystick(0);
+
+  /*
+   * Magic numbers. Use these to adjust settings.
+   */
+
+  /**
+   * How many amps the arm motor can use.
+   */
+  static final int ARM_CURRENT_LIMIT_A = 20;
+
+  /**
+   * Percent output to run the arm up/down at
+   */
+  static final double ARM_OUTPUT_POWER = 0.4;
+
+  /**
+   * How many amps the intake can use while picking up
+   */
+  static final int INTAKE_CURRENT_LIMIT_A = 25;
+
+  /**
+   * How many amps the intake can use while holding
+   */
+  static final int INTAKE_HOLD_CURRENT_LIMIT_A = 5;
+
+  /**
+   * Percent output for intaking
+   */
+  static final double INTAKE_OUTPUT_POWER = 1.0;
+
+  /**
+   * Percent output for holding
+   */
+  static final double INTAKE_HOLD_POWER = 0.07;
+
+  /**
+   * Time to extend or retract arm in auto
+   */
+  static final double ARM_EXTEND_TIME_S = 2.0;
+
+  /**
+   * Time to throw game piece in auto
+   */
+  static final double AUTO_THROW_TIME_S = 0.375;
+
+  /**
+   * Time to drive back in auto
+   */
+  static final double AUTO_DRIVE_TIME = 6.0;
+
+  /**
+   * Speed to drive backwards in auto
+   */
+  static final double AUTO_DRIVE_SPEED = -0.25;
+
+  /**
+   * This method is run once when the robot is first started up.
+   */
   @Override
   public void robotInit() {
-    // initialize motor
-    m_motor = new CANSparkMax(deviceID, MotorType.kBrushless);
+    m_chooser.setDefaultOption("do nothing", kNothingAuto);
+    m_chooser.addOption("cone and mobility", kConeAuto);
+    m_chooser.addOption("cube and mobility", kCubeAuto);
+    SmartDashboard.putData("Auto choices", m_chooser);
 
-    /**
-     * The RestoreFactoryDefaults method can be used to reset the configuration parameters
-     * in the SPARK MAX to their factory default state. If no argument is passed, these
-     * parameters will not persist between power cycles
-     */
-    m_motor.restoreFactoryDefaults();
-
-    // initialze PID controller and encoder objects
-    m_pidController = m_motor.getPIDController();
-    m_encoder = m_motor.getEncoder();
-
-    // PID coefficients
-    kP = 5e-5; 
-    kI = 1e-6;
-    kD = 0; 
-    kIz = 0; 
-    kFF = 0.000156; 
-    kMaxOutput = 1; 
-    kMinOutput = -1;
-    maxRPM = 5700;
-
-    // Smart Motion Coefficients
-    maxVel = 2000; // rpm
-    maxAcc = 1500;
-
-    // set PID coefficients
-    m_pidController.setP(kP);
-    m_pidController.setI(kI);
-    m_pidController.setD(kD);
-    m_pidController.setIZone(kIz);
-    m_pidController.setFF(kFF);
-    m_pidController.setOutputRange(kMinOutput, kMaxOutput);
-
-    /**
-     * Smart Motion coefficients are set on a SparkMaxPIDController object
+    /*
+     * You will need to change some of these from false to true.
      * 
-     * - setSmartMotionMaxVelocity() will limit the velocity in RPM of
-     * the pid controller in Smart Motion mode
-     * - setSmartMotionMinOutputVelocity() will put a lower bound in
-     * RPM of the pid controller in Smart Motion mode
-     * - setSmartMotionMaxAccel() will limit the acceleration in RPM^2
-     * of the pid controller in Smart Motion mode
-     * - setSmartMotionAllowedClosedLoopError() will set the max allowed
-     * error for the pid controller in Smart Motion mode
+     * In the setDriveMotors method, comment out all but 1 of the 4 calls
+     * to the set() methods. Push the joystick forward. Reverse the motor
+     * if it is going the wrong way. Repeat for the other 3 motors.
      */
-    int smartMotionSlot = 0;
-    m_pidController.setSmartMotionMaxVelocity(maxVel, smartMotionSlot);
-    m_pidController.setSmartMotionMinOutputVelocity(minVel, smartMotionSlot);
-    m_pidController.setSmartMotionMaxAccel(maxAcc, smartMotionSlot);
-    m_pidController.setSmartMotionAllowedClosedLoopError(allowedErr, smartMotionSlot);
+    driveLeftSpark.setInverted(false);
+    driveLeftSpark2.setInverted(false);
+    driveRightSpark.setInverted(true);
+    driveRightSpark2.setInverted(true);
 
-    // display PID coefficients on SmartDashboard
-    SmartDashboard.putNumber("P Gain", kP);
-    SmartDashboard.putNumber("I Gain", kI);
-    SmartDashboard.putNumber("D Gain", kD);
-    SmartDashboard.putNumber("I Zone", kIz);
-    SmartDashboard.putNumber("Feed Forward", kFF);
-    SmartDashboard.putNumber("Max Output", kMaxOutput);
-    SmartDashboard.putNumber("Min Output", kMinOutput);
+    /*
+     * Set the arm and intake to brake mode to help hold position.
+     * If either one is reversed, change that here too. Arm out is defined
+     * as positive, arm in is negative.
+     */
+    arm.setInverted(true);
+    arm.setIdleMode(IdleMode.kBrake);
+    arm.setSmartCurrentLimit(ARM_CURRENT_LIMIT_A);
+    intake.setInverted(false);
+    intake.setIdleMode(IdleMode.kBrake);
+  }
 
-    // display Smart Motion coefficients
-    SmartDashboard.putNumber("Max Velocity", maxVel);
-    SmartDashboard.putNumber("Min Velocity", minVel);
-    SmartDashboard.putNumber("Max Acceleration", maxAcc);
-    SmartDashboard.putNumber("Allowed Closed Loop Error", allowedErr);
-    SmartDashboard.putNumber("Set Position", 0);
-    SmartDashboard.putNumber("Set Velocity", 5);
+  /**
+   * Calculate and set the power to apply to the left and right
+   * drive motors.
+   * 
+   * @param forward Desired forward speed. Positive is forward.
+   * @param turn    Desired turning speed. Positive is counter clockwise from
+   *                above.
+   */
+  public void setDriveMotors(double forward, double turn) {
+    SmartDashboard.putNumber("drive forward power (%)", forward);
+    SmartDashboard.putNumber("drive turn power (%)", turn);
 
-    // button to toggle between velocity and smart motion modes
-    SmartDashboard.putBoolean("Mode", true);
+    /*
+     * positive turn = counter clockwise, so the left side goes backwards
+     */
+    double left = forward - turn;
+    double right = forward + turn;
+
+    SmartDashboard.putNumber("drive left power (%)", left);
+    SmartDashboard.putNumber("drive right power (%)", right);
+
+    // see note above in robotInit about commenting these out one by one to set
+    // directions.
+    driveLeftSpark.set(left);
+    driveLeftSpark2.set(left);
+    driveRightSpark.set(right);
+    driveRightSpark2.set(right);
+  }
+
+  /**
+   * Set the arm output power. Positive is out, negative is in.
+   * 
+   * @param percent
+   */
+  public void setArmMotor(double percent) {
+    arm.set(percent);
+    SmartDashboard.putNumber("arm power (%)", percent);
+    SmartDashboard.putNumber("arm motor current (amps)", arm.getOutputCurrent());
+    SmartDashboard.putNumber("arm motor temperature (C)", arm.getMotorTemperature());
+  }
+
+  /**
+   * Set the arm output power.
+   * 
+   * @param percent desired speed
+   * @param amps current limit
+   */
+  public void setIntakeMotor(double percent, int amps) {
+    intake.set(percent);
+    intake.setSmartCurrentLimit(amps);
+    SmartDashboard.putNumber("intake power (%)", percent);
+    SmartDashboard.putNumber("intake motor current (amps)", intake.getOutputCurrent());
+    SmartDashboard.putNumber("intake motor temperature (C)", intake.getMotorTemperature());
+  }
+
+  /**
+   * This method is called every 20 ms, no matter the mode. It runs after
+   * the autonomous and teleop specific period methods.
+   */
+  @Override
+  public void robotPeriodic() {
+    SmartDashboard.putNumber("Time (seconds)", Timer.getFPGATimestamp());
+  }
+
+  double autonomousStartTime;
+  double autonomousIntakePower;
+
+  @Override
+  public void autonomousInit() {
+    driveLeftSpark.setIdleMode(IdleMode.kBrake);
+    driveLeftSpark2.setIdleMode(IdleMode.kBrake);
+    driveRightSpark.setIdleMode(IdleMode.kBrake);
+    driveRightSpark2.setIdleMode(IdleMode.kBrake);;
+
+    m_autoSelected = m_chooser.getSelected();
+    System.out.println("Auto selected: " + m_autoSelected);
+
+    if (m_autoSelected == kConeAuto) {
+      autonomousIntakePower = INTAKE_OUTPUT_POWER;
+    } else if (m_autoSelected == kCubeAuto) {
+      autonomousIntakePower = -INTAKE_OUTPUT_POWER;
+    }
+
+    autonomousStartTime = Timer.getFPGATimestamp();
+  }
+
+  @Override
+  public void autonomousPeriodic() {
+    if (m_autoSelected == kNothingAuto) {
+      setArmMotor(0.0);
+      setIntakeMotor(0.0, INTAKE_CURRENT_LIMIT_A);
+      setDriveMotors(0.0, 0.0);
+      return;
+    }
+
+    double timeElapsed = Timer.getFPGATimestamp() - autonomousStartTime;
+
+    if (timeElapsed < ARM_EXTEND_TIME_S) {
+      setArmMotor(ARM_OUTPUT_POWER);
+      setIntakeMotor(0.0, INTAKE_CURRENT_LIMIT_A);
+      setDriveMotors(0.0, 0.0);
+    } else if (timeElapsed < ARM_EXTEND_TIME_S + AUTO_THROW_TIME_S) {
+      setArmMotor(0.0);
+      setIntakeMotor(autonomousIntakePower, INTAKE_CURRENT_LIMIT_A);
+      setDriveMotors(0.0, 0.0);
+    } else if (timeElapsed < ARM_EXTEND_TIME_S + AUTO_THROW_TIME_S + ARM_EXTEND_TIME_S) {
+      setArmMotor(-ARM_OUTPUT_POWER);
+      setIntakeMotor(0.0, INTAKE_CURRENT_LIMIT_A);
+      setDriveMotors(0.0, 0.0);
+    } else if (timeElapsed < ARM_EXTEND_TIME_S + AUTO_THROW_TIME_S + ARM_EXTEND_TIME_S + AUTO_DRIVE_TIME) {
+      setArmMotor(0.0);
+      setIntakeMotor(0.0, INTAKE_CURRENT_LIMIT_A);
+      setDriveMotors(AUTO_DRIVE_SPEED, 0.0);
+    } else {
+      setArmMotor(0.0);
+      setIntakeMotor(0.0, INTAKE_CURRENT_LIMIT_A);
+      setDriveMotors(0.0, 0.0);
+    }
+  }
+
+  /**
+   * Used to remember the last game piece picked up to apply some holding power.
+   */
+  static final int CONE = 1;
+  static final int CUBE = 2;
+  static final int NOTHING = 3;
+  int lastGamePiece;
+
+  @Override
+  public void teleopInit() {
+    driveLeftSpark.setIdleMode(IdleMode.kCoast);
+    driveLeftSpark2.setIdleMode(IdleMode.kCoast);
+    driveRightSpark.setIdleMode(IdleMode.kCoast);
+    driveRightSpark2.setIdleMode(IdleMode.kCoast);
+
+    lastGamePiece = NOTHING;
   }
 
   @Override
   public void teleopPeriodic() {
-    // read PID coefficients from SmartDashboard
-    double p = SmartDashboard.getNumber("P Gain", 0);
-    double i = SmartDashboard.getNumber("I Gain", 0);
-    double d = SmartDashboard.getNumber("D Gain", 0);
-    double iz = SmartDashboard.getNumber("I Zone", 0);
-    double ff = SmartDashboard.getNumber("Feed Forward", 0);
-    double max = SmartDashboard.getNumber("Max Output", 0);
-    double min = SmartDashboard.getNumber("Min Output", 0);
-    double maxV = SmartDashboard.getNumber("Max Velocity", 0);
-    double minV = SmartDashboard.getNumber("Min Velocity", 0);
-    double maxA = SmartDashboard.getNumber("Max Acceleration", 0);
-    double allE = SmartDashboard.getNumber("Allowed Closed Loop Error", 0);
-
-    // if PID coefficients on SmartDashboard have changed, write new values to controller
-    if((p != kP)) { m_pidController.setP(p); kP = p; }
-    if((i != kI)) { m_pidController.setI(i); kI = i; }
-    if((d != kD)) { m_pidController.setD(d); kD = d; }
-    if((iz != kIz)) { m_pidController.setIZone(iz); kIz = iz; }
-    if((ff != kFF)) { m_pidController.setFF(ff); kFF = ff; }
-    if((max != kMaxOutput) || (min != kMinOutput)) { 
-      m_pidController.setOutputRange(min, max); 
-      kMinOutput = min; kMaxOutput = max; 
+    if (j.getRawButton(1)) {
+      setArmMotor(.3);
     }
-    if((maxV != maxVel)) { m_pidController.setSmartMotionMaxVelocity(maxV,0); maxVel = maxV; }
-    if((minV != minVel)) { m_pidController.setSmartMotionMinOutputVelocity(minV,0); minVel = minV; }
-    if((maxA != maxAcc)) { m_pidController.setSmartMotionMaxAccel(maxA,0); maxAcc = maxA; }
-    if((allE != allowedErr)) { m_pidController.setSmartMotionAllowedClosedLoopError(allE,0); allowedErr = allE; }
-
-    double setPoint, processVariable;
-    boolean mode = SmartDashboard.getBoolean("Mode", false);
-    if(mode) {
-      setPoint = SmartDashboard.getNumber("Set Velocity", 0);
-      m_pidController.setReference(setPoint, CANSparkMax.ControlType.kVelocity);
-      processVariable = m_encoder.getVelocity();
-    } else {
-      setPoint = SmartDashboard.getNumber("Set Position", 0);
-      /**
-       * As with other PID modes, Smart Motion is set by calling the
-       * setReference method on an existing pid object and setting
-       * the control type to kSmartMotion
-       */
-      m_pidController.setReference(setPoint, CANSparkMax.ControlType.kSmartMotion);
-      processVariable = m_encoder.getPosition();
+    if(j.getRawButton(2)){
+      setArmMotor(-.3);
     }
     
-    SmartDashboard.putNumber("SetPoint", setPoint);
-    SmartDashboard.putNumber("Process Variable", processVariable);
-    SmartDashboard.putNumber("Output", m_motor.getAppliedOutput());
+
+    if(j.getRawButton(3)){
+      setIntakeMotor(1, 20);
+    }
+    if(j.getRawButton(4)){
+      setIntakeMotor(-1, 20);
+    }
+    if(j.getRawButton(5)){
+      setIntakeMotor(0, 20);
+    }
+
+    
+
+    /*
+     * Negative signs here because the values from the analog sticks are backwards
+     * from what we want. Forward returns a negative when we want it positive.
+     */
+    setDriveMotors(-j.getRawAxis(1)*.1, -j.getRawAxis(0)*.1);
   }
 }
