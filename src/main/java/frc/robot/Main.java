50f2c90bb6d8c9d31c31e7f5cb7cd34d40f41995
Akash Danda <mindstormakash@gmail.com>
2023-02-27 21:30:02 -0800

    added sample gyro code.

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -5,6 +5,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj.interfaces.Gyro;
 
 /**
  * Do NOT add any static variables to this class, or any initialization at all. Unless you know what
@@ -20,6 +21,6 @@ public final class Main {
    * <p>If you change your main robot class, change the parameter type.
    */
   public static void main(String... args) {
-    RobotBase.startRobot(RobotPID::new);
+    RobotBase.startRobot(gyro::new);
   }
 }
